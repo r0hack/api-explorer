f49dcf64247027ca5cdac4e835451812757c191b
Sean Dokko <dokko1230@gmail.com>
2019-07-01 10:21:05 -0700

    ♻️ convert to stateful components for SSR

--- packages/api-explorer/src/Example.jsx
+++ packages/api-explorer/src/Example.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 const PropTypes = require('prop-types');
-const ReactJson = require('react-json-view').default;
+// const ReactJson = require('react-json-view').default;
 const showCodeResults = require('./lib/show-code-results');
 const contentTypeIsJson = require('./lib/content-type-is-json');
 
@@ -14,62 +14,133 @@ const Oas = require('./lib/Oas');
 
 const { Operation } = Oas;
 
-function Example({ operation, result, oas, selected, setExampleTab, exampleResponses }) {
-  const examples = exampleResponses.length ? exampleResponses : showCodeResults(operation);
-  const hasExamples = examples.find(e => e.code && e.code !== '{}');
-  return (
-    <div className="hub-reference-results-examples code-sample">
-      {examples &&
-      examples.length > 0 &&
-      hasExamples && (
-        <span>
-          <ExampleTabs examples={examples} selected={selected} setExampleTab={setExampleTab} />
-          <div className="code-sample-body">
-            {examples.map((example, index) => {
-              const isJson = example.language && contentTypeIsJson(example.language);
-              return (
-                <pre
-                  className={`tomorrow-night tabber-body tabber-body-${index}`}
-                  style={{ display: index === selected ? 'block' : '' }}
-                  key={index} // eslint-disable-line react/no-array-index-key
-                >
-                  {isJson ? (
-                    <ReactJson
-                      src={JSON.parse(example.code)}
-                      collapsed={2}
-                      collapseStringsAfterLength={100}
-                      enableClipboard={false}
-                      theme="tomorrow"
-                      name={null}
-                      displayDataTypes={false}
-                      displayObjectSize={false}
-                      style={{
-                        padding: '20px 10px',
-                        backgroundColor: 'transparent',
-                        fontSize: '12px',
-                      }}
-                    />
-                  ) : (
-                    <div>{syntaxHighlighter(example.code, example.language, { dark: true })}</div>
-                  )}
-                </pre>
-              );
-            })}
+let ReactJson;
+class Example extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = { renderResponse: false };
+  }
+  componentDidMount() {
+    ReactJson = require('react-json-view').default;
+    this.setState({ renderResponse: true });
+  }
+  render() {
+    const { operation, result, oas, selected, setExampleTab, exampleResponses } = this.props;
+    const { renderResponse } = this.state;
+
+    const examples = exampleResponses.length ? exampleResponses : showCodeResults(operation);
+    const hasExamples = examples.find(e => e.code && e.code !== '{}');
+    return (
+      <div className="hub-reference-results-examples code-sample">
+        {examples &&
+        examples.length > 0 &&
+        hasExamples && (
+          <span>
+            <ExampleTabs examples={examples} selected={selected} setExampleTab={setExampleTab} />
+            <div className="code-sample-body">
+              {examples.map((example, index) => {
+                const isJson = example.language && contentTypeIsJson(example.language);
+                return (
+                  <pre
+                    className={`tomorrow-night tabber-body tabber-body-${index}`}
+                    style={{ display: index === selected ? 'block' : '' }}
+                    key={index} // eslint-disable-line react/no-array-index-key
+                  >
+                    {renderResponse && isJson ? (
+                      <ReactJson
+                        src={JSON.parse(example.code)}
+                        collapsed={2}
+                        collapseStringsAfterLength={100}
+                        enableClipboard={false}
+                        theme="tomorrow"
+                        name={null}
+                        displayDataTypes={false}
+                        displayObjectSize={false}
+                        style={{
+                          padding: '20px 10px',
+                          backgroundColor: 'transparent',
+                          fontSize: '12px',
+                        }}
+                      />
+                    ) : (
+                      <div>{syntaxHighlighter(example.code, example.language, { dark: true })}</div>
+                    )}
+                  </pre>
+                );
+              })}
+            </div>
+          </span>
+        )}
+        {(examples.length === 0 || (!hasExamples && result === null)) && (
+          <div className="hub-no-code">
+            {oas[extensions.EXPLORER_ENABLED] ? (
+              'Try the API to see Results'
+            ) : (
+              'No response examples available'
+            )}
           </div>
-        </span>
-      )}
-      {(examples.length === 0 || (!hasExamples && result === null)) && (
-        <div className="hub-no-code">
-          {oas[extensions.EXPLORER_ENABLED] ? (
-            'Try the API to see Results'
-          ) : (
-            'No response examples available'
-          )}
-        </div>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
+  }
 }
+
+// function Example({ operation, result, oas, selected, setExampleTab, exampleResponses }) {
+//   const examples = exampleResponses.length ? exampleResponses : showCodeResults(operation);
+//   const hasExamples = examples.find(e => e.code && e.code !== '{}');
+//   return (
+//     <div className="hub-reference-results-examples code-sample">
+//       {examples &&
+//       examples.length > 0 &&
+//       hasExamples && (
+//         <span>
+//           <ExampleTabs examples={examples} selected={selected} setExampleTab={setExampleTab} />
+//           <div className="code-sample-body">
+//             {examples.map((example, index) => {
+//               const isJson = example.language && contentTypeIsJson(example.language);
+//               return (
+//                 <pre
+//                   className={`tomorrow-night tabber-body tabber-body-${index}`}
+//                   style={{ display: index === selected ? 'block' : '' }}
+//                   key={index} // eslint-disable-line react/no-array-index-key
+//                 >
+//                   {isJson ? (
+//                     <ReactJson
+//                       src={JSON.parse(example.code)}
+//                       collapsed={2}
+//                       collapseStringsAfterLength={100}
+//                       enableClipboard={false}
+//                       theme="tomorrow"
+//                       name={null}
+//                       displayDataTypes={false}
+//                       displayObjectSize={false}
+//                       style={{
+//                         padding: '20px 10px',
+//                         backgroundColor: 'transparent',
+//                         fontSize: '12px',
+//                       }}
+//                     />
+//                   ) : (
+//                     <div>{syntaxHighlighter(example.code, example.language, { dark: true })}</div>
+//                   )}
+//                 </pre>
+//               );
+//             })}
+//           </div>
+//         </span>
+//       )}
+//       {(examples.length === 0 || (!hasExamples && result === null)) && (
+//         <div className="hub-no-code">
+//           {oas[extensions.EXPLORER_ENABLED] ? (
+//             'Try the API to see Results'
+//           ) : (
+//             'No response examples available'
+//           )}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 
 module.exports = Example;
 
--- packages/api-explorer/src/ResponseBody.jsx
+++ packages/api-explorer/src/ResponseBody.jsx
@@ -1,45 +1,94 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const syntaxHighlighter = require('@readme/syntax-highlighter');
-const ReactJson = require('react-json-view').default;
 const contentTypeIsJson = require('./lib/content-type-is-json');
 const oauthHref = require('./lib/oauth-href');
 
-function Authorized({ result }) {
-  const isJson =
-    result.type && contentTypeIsJson(result.type) && typeof result.responseBody === 'object';
-  return (
-    <div>
-      {result.isBinary && <div>A binary file was returned</div>}
-      {!result.isBinary &&
-      isJson && (
-        <ReactJson
-          src={result.responseBody}
-          collapsed={1}
-          collapseStringsAfterLength={100}
-          enableClipboard={false}
-          theme="tomorrow"
-          name={null}
-          displayDataTypes={false}
-          displayObjectSize={false}
-          style={{
-            padding: '20px 10px',
-            backgroundColor: 'transparent',
-            fontSize: '12px',
-          }}
-        />
-      )}
-      {!result.isBinary &&
-      !isJson && (
-        <pre className="tomorrow-night">
-          <div className="cm-s-tomorrow-night codemirror-highlight">
-            {syntaxHighlighter(result.responseBody, result.type)}
-          </div>
-        </pre>
-      )}
-    </div>
-  );
+
+let ReactJson;
+class Authorized extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = { renderResponse: false };
+  }
+  componentDidMount() {
+    ReactJson = require('react-json-view').default;
+    this.setState({ renderResponse: true });
+  }
+  render() {
+    const { result } = this.props;
+    const { renderResponse } = this.state;
+    const isJson =
+      result.type && contentTypeIsJson(result.type) && typeof result.responseBody === 'object';
+    return (
+      <div>
+        {result.isBinary && <div>A binary file was returned</div>}
+        {!result.isBinary &&
+        isJson && renderResponse && (
+          <ReactJson
+            src={result.responseBody}
+            collapsed={1}
+            collapseStringsAfterLength={100}
+            enableClipboard={false}
+            theme="tomorrow"
+            name={null}
+            displayDataTypes={false}
+            displayObjectSize={false}
+            style={{
+              padding: '20px 10px',
+              backgroundColor: 'transparent',
+              fontSize: '12px',
+            }}
+          />
+        )}
+        {!result.isBinary &&
+        !isJson && (
+          <pre className="tomorrow-night">
+            <div className="cm-s-tomorrow-night codemirror-highlight">
+              {syntaxHighlighter(result.responseBody, result.type)}
+            </div>
+          </pre>
+        )}
+      </div>
+    );
+  }
 }
+
+// function Authorized({ result }) {
+//   const isJson =
+//     result.type && contentTypeIsJson(result.type) && typeof result.responseBody === 'object';
+//   return (
+//     <div>
+//       {result.isBinary && <div>A binary file was returned</div>}
+//       {!result.isBinary &&
+//       isJson && (
+//         <ReactJson
+//           src={result.responseBody}
+//           collapsed={1}
+//           collapseStringsAfterLength={100}
+//           enableClipboard={false}
+//           theme="tomorrow"
+//           name={null}
+//           displayDataTypes={false}
+//           displayObjectSize={false}
+//           style={{
+//             padding: '20px 10px',
+//             backgroundColor: 'transparent',
+//             fontSize: '12px',
+//           }}
+//         />
+//       )}
+//       {!result.isBinary &&
+//       !isJson && (
+//         <pre className="tomorrow-night">
+//           <div className="cm-s-tomorrow-night codemirror-highlight">
+//             {syntaxHighlighter(result.responseBody, result.type)}
+//           </div>
+//         </pre>
+//       )}
+//     </div>
+//   );
+// }
 
 Authorized.propTypes = {
   result: PropTypes.shape({}).isRequired,
